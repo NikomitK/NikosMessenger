82fdda80a0a49ce2d91832c98fd63a368260590b
Niko <nikobo2003@gmail.com>
2021-12-12 20:26:29 +0100

    fixed import ig

--- src/main/java/tk/nikomitk/gui/login/LoginScreen.java
+++ src/main/java/tk/nikomitk/gui/login/LoginScreen.java
@@ -5,7 +5,6 @@ import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginScreen extends JFrame {
