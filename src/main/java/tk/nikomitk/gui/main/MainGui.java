7243c61c98c1b3bbfdd9403d3067181ebdf33e9c
Niko <nikobo2003@gmail.com>
2021-12-12 20:27:11 +0100

    added a bit of structure for main gui element

---
+++ src/main/java/tk/nikomitk/gui/main/MainGui.java
@@ -0,0 +1,18 @@
+package tk.nikomitk.gui.main;
+
+import tk.nikomitk.gui.main.chat.ChatPanel;
+import tk.nikomitk.gui.main.chatlist.ChatListPanel;
+
+import javax.swing.*;
+
+public class MainGui {
+
+    private JPanel mainPanel;
+    private ChatListPanel chatListPanel;
+    private ChatPanel chatPanel;
+
+    public MainGui(){
+        // TODO "add new chat" as button underneath the chats, maybe settings above chatlist like whatsapp web
+    }
+
+}
