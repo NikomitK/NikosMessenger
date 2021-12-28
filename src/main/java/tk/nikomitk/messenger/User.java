051c6c5c29d6542907bf0fec5a4de0aa3dd261ee
Niko <nikobo2003@gmail.com>
2021-12-28 09:36:13 +0100

    some stuff for serializing

--- src/main/java/tk/nikomitk/messenger/User.java
+++ src/main/java/tk/nikomitk/messenger/User.java
@@ -1,8 +1,15 @@
 package tk.nikomitk.messenger;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private int profilePicture;
@@ -14,6 +21,17 @@ public class User {
         this.profilePicture = profilePicture;
         this.userName = userName;
         this.nickName = userName;
+    }
+
+    @JsonIgnore
+    public String getJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode messageNode = objectMapper.valueToTree(this);
+        try {
+            return objectMapper.writer().writeValueAsString(messageNode);
+        } catch (Exception e) {
+            return "not availible";
+        }
     }
 
 }
