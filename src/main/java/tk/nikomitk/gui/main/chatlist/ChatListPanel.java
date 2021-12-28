1a8d0675bfc1416bf5d6265aeaa1c7248ab3f8c0
Niko <nikobo2003@gmail.com>
2021-12-28 09:16:10 +0100

    cleanup add chatlisting if someone new texts you

--- src/main/java/tk/nikomitk/gui/main/chatlist/ChatListPanel.java
+++ src/main/java/tk/nikomitk/gui/main/chatlist/ChatListPanel.java
@@ -1,37 +1,61 @@
 package tk.nikomitk.gui.main.chatlist;
 
 import net.miginfocom.swing.MigLayout;
+import tk.nikomitk.Main;
+import tk.nikomitk.gui.utils.UtilMethods;
+import tk.nikomitk.messenger.Message;
 import tk.nikomitk.messenger.User;
 
 import javax.swing.*;
 import java.awt.*;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class ChatListPanel extends JPanel {
 
-    private final JPanel mainPanel;
-    private final JScrollPane scrollPane;
-    private final JPanel contentPanel;
     private final List<ChatListing> openChatList;
+    private JPanel mainPanel;
+    private JScrollPane scrollPane;
+    private JPanel contentPanel;
+    private AddListingButton addListingButton;
 
     public ChatListPanel() {
         openChatList = new ArrayList<>();
-        setLayout(new GridLayout());
+        createLook();
+        initComponents();
+    }
 
-        mainPanel = new JPanel(new GridLayout());
+    private void createLook() {
+        setLayout(new GridLayout());
+        setPreferredSize(new Dimension(400, 10000));
+        setBackground(Main.getColor("PanelBackground"));
+    }
+
+    private void initComponents() {
+        mainPanel = new JPanel(new MigLayout("", "[grow,fill]"));
+        Color backgroundColor = Main.getColor("PanelBackground");
+        mainPanel.setBackground(backgroundColor);
         add(mainPanel);
 
         // documentation for miglayout is terrible, but it's the easiest way
-        contentPanel = new JPanel(new MigLayout("", "[grow,fill]"));
+        contentPanel = new JPanel(new MigLayout("btt", "[grow,fill]"));
+        contentPanel.setOpaque(false);
         scrollPane = new JScrollPane(contentPanel);
-        mainPanel.add(scrollPane);
+        scrollPane.setOpaque(false);
+        scrollPane.setBorder(null);
+        scrollPane.getViewport().setOpaque(false);
+        mainPanel.add(scrollPane, "dock north");
     }
 
     public void addChatListing(User chatPartner, String lastMessage) {
         try {
             ChatListing tempListing = new ChatListing(chatPartner, lastMessage);
+            for (int i = 0; i < openChatList.size(); i++) {
+                if (openChatList.get(i).getChatPartner().getUserName().equals(chatPartner.getUserName()))
+                    throw new InvalidParameterException("Chat already exists");
+            }
             openChatList.add(tempListing);
             contentPanel.add(tempListing, "wrap");
         } catch (Exception e) {
@@ -47,17 +71,14 @@ public class ChatListPanel extends JPanel {
         }
     }
 
-    public void changeLastMessage(User chatPartner, String message) {
+    public void changeLastMessage(Message message) {
         // Call this when the messages get updated and there are new ones
+        User chatPartner = UtilMethods.getChatPartner(message);
         for (ChatListing c : openChatList) {
             // returns a boolean if the criteria fit
             if (c.changeLastMessage(chatPartner, message)) return;
         }
-    }
-
-    private void createChatlisting() {
-        // TODO add a button with the same size of a chatlisting at the bottom to start a new chat
-
+        addChatListing(chatPartner, message.getText());
     }
 
 }
