a07cb8c85e2fa84eb70dbb8721f7e5b4de608a4a
Niko <nikobo2003@gmail.com>
2021-12-28 09:17:51 +0100

    added functionality

--- src/main/java/tk/nikomitk/gui/main/chat/ChatPanel.java
+++ src/main/java/tk/nikomitk/gui/main/chat/ChatPanel.java
@@ -1,29 +1,82 @@
 package tk.nikomitk.gui.main.chat;
 
+import lombok.Getter;
+import tk.nikomitk.Main;
+import tk.nikomitk.gui.main.MainGui;
+import tk.nikomitk.messenger.Chat;
+import tk.nikomitk.messenger.Message;
 import tk.nikomitk.messenger.User;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.List;
 
 public class ChatPanel extends JPanel {
-    // TODO for textfield to chat maybe custom button in custom textfield.
-    // TODO maybe miglayout, messagefield resizable
-    // TODO second constructor for chats that already exist, maybe jsonnode as parameter, unsure how chats will be stored
-    // TODO define color schemes in properties, messages, background, top-bar, etc
 
-    private User chatPartner;
+    @Getter
+    private final User chatPartner;
+    @Getter
+    private final Chat chat;
     private JPanel mainPanel;
-    private JPanel chatPanel;
-    private JPanel messagePanel;
-    private JTextField messageField;
-    private JButton sendButton;
-    private List<MessageContainer> messages; //maybe don't need this
+    private ChatPartnerBar chatPartnerBar;
+    private MessagePanel messagePanel;
+    private MessageBar messageBar;
+    private List<MessageContainer> messages;
 
-    public ChatPanel(User chatPartner){
+    public ChatPanel(User chatPartner) throws IOException {
         this.chatPartner = chatPartner;
-        mainPanel = new JPanel(new BorderLayout());
+        createLook();
+        initComponents();
+        chat = Main.getChat(this.chatPartner.getUserName());
+        createHistory(chat);
+    }
 
+    private void createLook() {
+        setLayout(new GridLayout());
+    }
+
+    private void initComponents() throws IOException {
+        mainPanel = new JPanel(new BorderLayout());
+        add(mainPanel);
+
+        chatPartnerBar = new ChatPartnerBar(chatPartner);
+        mainPanel.add(chatPartnerBar, BorderLayout.PAGE_START);
+
+        messagePanel = new MessagePanel();
+        mainPanel.add(messagePanel, BorderLayout.CENTER);
+
+        messageBar = new MessageBar(this);
+        mainPanel.add(messageBar, BorderLayout.PAGE_END);
+    }
+
+    public void addMyMessage(String text) {
+        Message message = new Message(text, chatPartner);
+        messagePanel.addMessage(message, true);
+        Main.sendMessageToSocket(message);
+        chat.addMessage(message);
+        MainGui.newMessage(message);
+    }
+
+    public void addForeignMessage(Message message) {
+        messagePanel.addMessage(message, false);
+    }
+
+    public void deleteMessage(Message message) {
+        chat.deleteMessage(message);
+        saveChat();
+        messagePanel.deleteMessage(message);
+        messagePanel.revalidate();
+    }
+
+    private void createHistory(Chat chat) {
+        for (Message m : chat.getMessages()) {
+            messagePanel.addMessage(m, m.getSender().equals(Main.getUser().getUserName()));
+        }
+    }
+
+    public void saveChat() {
+        Main.saveChat(chat);
     }
 
 }
