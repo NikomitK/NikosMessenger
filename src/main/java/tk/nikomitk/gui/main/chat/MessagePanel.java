360aa0f925e909e2c3eb6c0cbebe1f2f4d3dac59
Niko <nikobo2003@gmail.com>
2021-12-28 09:29:16 +0100

    added "delete" functionality

--- src/main/java/tk/nikomitk/gui/main/chat/MessagePanel.java
+++ src/main/java/tk/nikomitk/gui/main/chat/MessagePanel.java
@@ -1,6 +1,7 @@
 package tk.nikomitk.gui.main.chat;
 
 import net.miginfocom.swing.MigLayout;
+import tk.nikomitk.Main;
 import tk.nikomitk.gui.utils.SmartScroller;
 import tk.nikomitk.messenger.Message;
 
@@ -9,7 +10,7 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-//TODO selectable background background
+//TODO selectable background
 public class MessagePanel extends JPanel {
 
     private final JPanel mainPanel;
@@ -19,11 +20,15 @@ public class MessagePanel extends JPanel {
 
     public MessagePanel() {
         setLayout(new GridLayout());
+
         mainPanel = new JPanel(new GridLayout());
         add(mainPanel);
 
         contentPanel = new JPanel(new MigLayout("", "[grow,fill]"));
+        contentPanel.setBackground(Main.getColor("ChatBackground"));
+        contentPanel.setOpaque(true);
         scrollPane = new JScrollPane(contentPanel);
+        scrollPane.setBorder(null);
         new SmartScroller(scrollPane); // see comments in class
         mainPanel.add(scrollPane);
         messages = new ArrayList<>();
@@ -34,7 +39,15 @@ public class MessagePanel extends JPanel {
         messages.add(thisMessageContainer);
         contentPanel.add(thisMessageContainer, "wrap");
         contentPanel.revalidate();
-        // If this doesnt work, revalidate in chatpanel
     }
 
+    public void deleteMessage(Message message) {
+        for (MessageContainer mc : messages) {
+            if (mc.isEqual(message)) {
+                contentPanel.remove(mc);
+                contentPanel.revalidate();
+                return;
+            }
+        }
+    }
 }
