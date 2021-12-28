de8457e32c9499880b416877cc54ac18a06634de
Niko <nikobo2003@gmail.com>
2021-12-28 09:35:20 +0100

    added functionality

--- src/main/java/tk/nikomitk/gui/login/SignUpPanel.java
+++ src/main/java/tk/nikomitk/gui/login/SignUpPanel.java
@@ -1,5 +1,7 @@
 package tk.nikomitk.gui.login;
 
+import lombok.Getter;
+import tk.nikomitk.Main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
@@ -17,12 +19,17 @@ public class SignUpPanel extends JPanel {
     private JCheckBox rememberMeBox;
     private JCheckBox showPasswordBox;
     private JPanel buttonPanel;
+    @Getter
     private JButton signUpButton;
     private JButton switchToLogInButton;
 
-    public SignUpPanel(ActionListener switchToLogIn, ActionListener signUpListener){
+    public SignUpPanel(ActionListener switchToLogIn, ActionListener signUpListener) {
+        initComponents(switchToLogIn, signUpListener);
+    }
+
+    private void initComponents(ActionListener switchToLogIn, ActionListener signUpListener) {
         setLayout(new GridLayout());
-        mainPanel = new JPanel(new GridLayout(8,1));
+        mainPanel = new JPanel(new GridLayout(8, 1));
         add(mainPanel);
 
         usernameLabel = new JLabel("    Username");
@@ -43,11 +50,12 @@ public class SignUpPanel extends JPanel {
         repeatPasswordField = new JPasswordField();
         mainPanel.add(repeatPasswordField);
 
-        checkBoxPanel = new JPanel(new GridLayout(1,2));
+        checkBoxPanel = new JPanel(new GridLayout(1, 2));
         mainPanel.add(checkBoxPanel);
 
         rememberMeBox = new JCheckBox();
         rememberMeBox.setText("Remember me!");
+        rememberMeBox.addChangeListener(e -> Main.settings.setRememberMe(rememberMeBox.isSelected()));
         checkBoxPanel.add(rememberMeBox);
 
         showPasswordBox = new JCheckBox();
@@ -55,10 +63,11 @@ public class SignUpPanel extends JPanel {
         checkBoxPanel.add(showPasswordBox);
 
 
-        buttonPanel = new JPanel(new GridLayout(1,2));
+        buttonPanel = new JPanel(new GridLayout(1, 2));
         mainPanel.add(buttonPanel);
 
-        signUpButton = new JButton("Log me in!");
+        signUpButton = new JButton("Sign me in!");
+        signUpButton.setFocusable(false);
         signUpButton.putClientProperty("JButton.buttonType", "roundRect");
         signUpButton.addActionListener(signUpListener);
         buttonPanel.add(signUpButton);
@@ -67,6 +76,25 @@ public class SignUpPanel extends JPanel {
         switchToLogInButton.putClientProperty("JButton.buttonType", "roundRect");
         switchToLogInButton.addActionListener(switchToLogIn);
         buttonPanel.add(switchToLogInButton);
+    }
+
+    public String getUsername() {
+        return usernameField.getText();
+    }
+
+    public String getPassword() {
+        return passwordField.getText();
+    }
+
+    public String getRepeatedPassword() {
+        return repeatPasswordField.getText();
+    }
+
+    public void passwordsDoNotMatch() {
+        passwordField.putClientProperty("JComponent.outline", Color.red);
+        repeatPasswordField.putClientProperty("JComponent.outline", Color.red);
+        this.revalidate();
+        JOptionPane.showMessageDialog(this, "Passwords do not match!");
     }
 
 }
