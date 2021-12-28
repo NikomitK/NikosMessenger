0c8485b037c1b3fce96b1d4996b3241b858f3012
Niko <nikobo2003@gmail.com>
2021-12-28 09:24:37 +0100

    fixed stupid constructor, added another one

--- src/main/java/tk/nikomitk/messenger/Message.java
+++ src/main/java/tk/nikomitk/messenger/Message.java
@@ -1,12 +1,22 @@
 package tk.nikomitk.messenger;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import tk.nikomitk.Main;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-// TODO copy most of prototype message class
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Message {
     private String sender;
     private String receiver;
@@ -14,17 +24,36 @@ public class Message {
     private String sentDate;
     private String type;
 
-    // INPUT NEEDS A :, probably useless now
-    public Message(String input, String username) {
-        String[] test = input.split("[.:]");
-        type = text;
-        receiver = test[0];
-        text = test[1];
-        this.sender = username;
-        sentDate = new SimpleDateFormat("dd/MM/yy HH:mm").format(new Date());
+    public Message(String input, String receiver) {
+        this.receiver = receiver;
+        text = input;
+        this.sender = Main.getUser().getUserName();
+        sentDate = new SimpleDateFormat("dd/MM/yy-HH:mm").format(new Date());
     }
 
+    public Message(String text, User receiver) {
+        // probably unnecessary
+        this.sender = Main.getUser().getUserName();
+        this.receiver = receiver.getUserName();
+        this.text = text;
+        this.sentDate = new SimpleDateFormat("dd/MM/yy-HH:mm").format(new Date());
+    }
+
+    @JsonIgnore
     public String getTime() {
-        return sentDate.split(" ")[1];
+        System.out.println(sentDate);
+        return sentDate.split("-")[1];
+    }
+
+    // idk how to turn this into an outputstream
+    public String toJson() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode messageNode = objectMapper.valueToTree(this);
+        return objectMapper.writer().writeValueAsString(messageNode);
+    }
+
+    public String[] toOutputStringArray() throws JsonProcessingException {
+        // TODO encrypt
+        return new String[]{"message", receiver, toJson()};
     }
 }
