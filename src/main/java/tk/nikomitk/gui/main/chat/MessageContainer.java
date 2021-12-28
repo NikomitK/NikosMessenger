b644d369dc5180377ae7783c2d78406f255a8a3d
Niko <nikobo2003@gmail.com>
2021-12-28 09:27:49 +0100

    I just realized, this probably contains an error because the message in this doesn't get changed when you edit the GraphicMessage

--- src/main/java/tk/nikomitk/gui/main/chat/MessageContainer.java
+++ src/main/java/tk/nikomitk/gui/main/chat/MessageContainer.java
@@ -2,31 +2,52 @@ package tk.nikomitk.gui.main.chat;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
 import net.miginfocom.swing.MigLayout;
+import tk.nikomitk.Main;
 import tk.nikomitk.messenger.Message;
 
 import javax.swing.*;
 import java.awt.*;
 
 // TODO make message accessible for editing
-// TODO selectable colors (and choose better default colors)
+// TODO selectable colors (and choose better default colors, maybe light-blue), (custom)properties for enemycolor and own color
 public class MessageContainer extends JPanel {
 
-    public MessageContainer(Message message, boolean isFromMe) {
+    private final GraphicMessage graphicMessage;
+    private final Message message;
 
-        setMinimumSize(new Dimension(40, 35));
-        setMaximumSize(new Dimension(10000, 10000));
+    public MessageContainer(Message message, boolean isFromMe) {
+        createLook();
         FlatDarculaLaf.setup();
+        this.message = message;
         // Still no clue how to use this layout
-        setLayout(new MigLayout("", "[grow,fill]"));
-        Color color = Color.decode("#444444");
+        String fromWho = "EMB";
         String orientation = "dock west";
         if (isFromMe) {
-            color = Color.decode("#2A474B");
+            fromWho = "FMB";
             orientation = "dock east";
         }
+        Color color = Main.getColor(fromWho);
         UIManager.put("Button.disabledBackground", color);
-        GraphicMessage thisMessageLabel = new GraphicMessage(message);
-        add(thisMessageLabel, orientation);
+        graphicMessage = new GraphicMessage(message);
+        add(graphicMessage, orientation);
+    }
+
+    private void createLook() {
+        UIManager.put("Button.arc", 25);
+        setMinimumSize(new Dimension(40, 35));
+        setMaximumSize(new Dimension(10000, 10000));
+        setLayout(new MigLayout("", "[grow,fill]"));
+        setOpaque(false);
+    }
+
+    public void changeColor(Color color) {
+        // only used in test for now
+        UIManager.put("Button.disabledBackground", color);
+        SwingUtilities.updateComponentTreeUI(this);
+    }
+
+    public boolean isEqual(Message message) {
+        return message.equals(this.message);
     }
 
 }
